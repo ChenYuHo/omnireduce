72ce01bffc84ab27daa548c2b904a47a1db76f9e
Chen-Yu Ho <chenyu.ho@kaust.edu.sa>
2021-05-19 07:12:09 +0000

    Correctly pin protobuf to 3.8.0

--- prepare.sh
+++ prepare.sh
@@ -3,5 +3,10 @@ set -e
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 git submodule init
 git submodule update --init "$@" --recursive
-cd $SCRIPTPATH/pytorch && git apply $SCRIPTPATH/pytorch.patch
-cd $SCRIPTPATH/gloo && git apply $SCRIPTPATH/gloo.patch
+cd $SCRIPTPATH/pytorch
+git apply $SCRIPTPATH/pytorch.patch
+git rm third_party/gloo
+cd third_party/protobuf
+git checkout 09745575a923640154bcf307fba8aedff47f240a
+cd $SCRIPTPATH/gloo
+git apply $SCRIPTPATH/gloo.patch
