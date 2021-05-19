a66495deba925a5bfe29d5af0d277921228c561e
Chen-Yu Ho <chenyu.ho@kaust.edu.sa>
2021-05-19 06:45:47 +0000

    Remove trailing spaces, apply patch in prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
 set -e
-
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 git submodule init
 git submodule update --init "$@" --recursive
+cd $SCRIPTPATH/pytorch && git apply $SCRIPTPATH/pytorch.patch
+cd $SCRIPTPATH/gloo && git apply $SCRIPTPATH/gloo.patch
